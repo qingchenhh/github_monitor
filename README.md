122a78c14e4205cb31ad40907743f3e91823bc94
清晨 <37011241+qingchenhh@users.noreply.github.com>
2024-10-09 15:00:30 +0800

    update

--- README.md
+++ README.md
@@ -1,14 +1,11 @@
 # GitHub工具监测小脚本
 
-其实网上有好些脚本了，比如yhy0大佬的：https://github.com/yhy0/github-cve-monitor
+推送支持server酱、企业微信机器人、钉钉机器人。
 
-我是参考了他的工具，因为我原先也不知道GitHub更新机制可以从GitHub的api中获取到信息，然后判断。
+优点：
 
----
-
-为啥有这个工具？就是当时使用yhy0大佬的工具的时候我只是单纯的想监控工具，不想监控什么关键字呀什么用户呀之类的。然后还发先跑起来的时候有两个工具一直在重复推送消息给我（后来发现是同名工具导致的），然后就想自己写一个，毕竟大佬的代码太复杂了不想看呀，其次也想自己尝试一下写写。
-
-我这个工具就比较low了，只能做到工具监测，而且只能server酱和企业微信机器人推送。
+1. 不同作者的同名工具，不会冲突，引发混乱推送。
+2. 项目404或者项目删除了release版本也会推送。
 
 # 工具的使用
 
@@ -20,19 +17,22 @@ pip install requests -i https://pypi.tuna.tsinghua.edu.cn/simple/ --trusted-host
 
 然后把工具一行一个的写道tools.txt中。
 
-配置一下GitHub的token和server酱的SendKey就好了或者企业微信机器人的key。
+配置一下GitHub的token和server酱、企业微信机器人、钉钉机器人其中一种的SendKey就好了。
 
 github的token怎么获取可以百度。
 
-![1679386101670](images/1679386101670.png)
+![1728456791758](images/1728456791758.png)
 
-最后使用放到服务器上，用nohup在后台跑着就行了，命令如下：
+最后使用放到服务器上，用使用计划任务跑着就行了，因为我监测的工具列表有点多，因此我设置为1小时执行一次，如果时间太短，上一次没执行完，下一次就开始执行了，不好，反正工具监测嘛，有实时性，但是没有必要那么精准，命令如下：
 
 ```
-nohup python3 github_monitor.py &
+0 * * * * python3 /root/qingchen/github_monitor/github_monitor-me.py >> /root/qingchen/github_monitor/results.txt 2>&1
 ```
 
 # 测试结果
 
-![1679386529979](images/1679386529979.png)
+![1728456968646](images/1728456968646.png)
 
+![1728456937584](images/1728456937584.png)
+
+![1728456982290](images/1728456982290.png)
--- github_monitor.py
+++ github_monitor.py
@@ -5,22 +5,21 @@ warnings.filterwarnings('ignore')
 
 # 以下需要配置以下！
 # -----------------------------------
-tools_path = "tools.txt" # 工具列表文件
+tools_path = "/root/qingchen/github_monitor/tools.txt" # 工具列表文件
+db = "/root/qingchen/github_monitor/data.db" # 数据库文件存放路径
 github_token = "" # GitHub token
-server_key = "" # server酱的SendKey
-webhook_key="" # 企业微信机器人key
-send_type = "Webhook" # 指定推送方式：ServerChan(server酱）、Webhook(企业微信机器人)
+SendKey="" # 配置使用机器人的key
+send_type = "Webhook" # 指定推送方式：ServerChan(server酱）、Webhook(企业微信机器人)、dingding（钉钉机器人）
 # -----------------------------------
 
-list_404 = []
 def create_db():
-    if os.path.exists('data.db'):
+    if os.path.exists(db):
         print('数据库已存在！')
         # os.remove('data.db')
-        conn = sqlite3.connect('data.db')
+        conn = sqlite3.connect(db)
         return conn.cursor(),conn
         # return True
-    conn = sqlite3.connect('data.db')
+    conn = sqlite3.connect(db)
     c = conn.cursor()
     try:
         c.execute('''CREATE TABLE IF NOT EXISTS tools
@@ -29,7 +28,9 @@ def create_db():
                 tools_url varchar(255),
                 releases varchar(255),
                 releases_time varchar(255),
-                author varchar(255));''')
+                author varchar(255)),
+                is_del varchar(255),
+                release_is_del varchar(255));''')
         print("工具表创建成功！")
     except Exception as e:
         print("["+time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())+"]","工具表创建出错啦！报错：",e)
@@ -53,13 +54,13 @@ def get_github_data(url):
     }
     dic = {}
     try:
-        rep1 = requests.get(url, headers=headers, verify=False).json()
+        rep1 = requests.get(url, headers=headers, verify=False, timeout=10).json()
         time.sleep(1.5)
         # print(rep1)
         dic['tools_name'] = rep1['name']
         dic['tools_url'] = rep1['html_url']
         dic['author'] = rep1['owner']['login']
-        rep = requests.get(url+"/commits", headers=headers, verify=False).json()
+        rep = requests.get(url+"/commits", headers=headers, verify=False, timeout=10).json()
         time.sleep(1.5)
         dic['html_url'] = rep[0]['html_url']
         commit_date = rep[0]['commit']['committer']['date']
@@ -72,7 +73,7 @@ def get_github_data(url):
         dic['releases'] = "0"
         dic['releases_time'] = "0"
         dic['releases_url'] = "0"
-        release_rep = requests.get(url+"/releases", headers=headers, verify=False).json()
+        release_rep = requests.get(url+"/releases", headers=headers, verify=False, timeout=10).json()
         time.sleep(1.5)
         if release_rep :
             dic['releases'] = release_rep[0]['tag_name']
@@ -84,19 +85,22 @@ def get_github_data(url):
     except Exception as e:
         e_str = str(e)
         if "<!DOCTYPE html>" not in e_str:
-            # print("["+time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())+"]","获取GitHub API数据时出错啦！报错：", e, url)
-            rep_err = requests.get(url, headers=headers, verify=False).json()
-            if rep_err['message'] == "Not Found":
-                print("==============ERROR==============")
-                print("[" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + "]", "该项目已经404!!!")
-                print("==============ERROR==============")
-                list_404.append(url)
-                send_server('！！！项目404告警！！！', '工具地址：' + url.replace('https://api.github.com/repos/','https://github.com/') + " ，\n监测地址：" + url)
-            else:
-                print("==============ERROR==============")
+            try:
+                # print("["+time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())+"]","获取GitHub API数据时出错啦！报错：", e, url)
+                rep_err = requests.get(url, headers=headers, verify=False, timeout=10).json()
+                if rep_err['message'] == "Not Found":
+                    print("==============ERROR==============")
+                    print("[" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + "]", "该项目已经404!!!")
+                    print("==============ERROR==============")
+                    return 404
+                else:
+                    print("==============ERROR==============")
+                    print("[" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + "]", "获取GitHub API数据时出错啦！报错：", e,
+                          url)
+                    print("==============ERROR==============")
+            except Exception as e1:
                 print("[" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + "]", "获取GitHub API数据时出错啦！报错：", e,
                       url)
-                print("==============ERROR==============")
         return False
 
 def send_server(title,msg):
@@ -104,10 +108,10 @@ def send_server(title,msg):
     print(title)
     print(msg)
     if send_type == "ServerChan":
-        if server_key != "":
+        if SendKey != "":
             try:
                 data = {"title":title,"desp":msg}
-                url = 'https://sc.ftqq.com/{}.send'.format(server_key)
+                url = 'https://sc.ftqq.com/{}.send'.format(SendKey)
                 requests.post(url,data=data,verify=False)
             except Exception as e:
                 print("["+time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())+"]",e)
@@ -115,84 +119,122 @@ def send_server(title,msg):
             print('未指定server酱的key，请配置！')
             exit()
     elif send_type == "Webhook":
-        if webhook_key != "":
+        if SendKey != "":
             try:
                 headers = {
                     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36',
                     'Content-Type': "application/json"
                 }
-                msg = msg.replace('更新消息为：', '更新消息为：\n')
-                msg = msg.replace('\n', '\n>')
-                data = {"msgtype": "markdown","markdown": {"content":title + "\n" + ">" + msg}}
-                url = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key={}'.format(webhook_key)
+                msg = msg.replace('更新消息为：','更新消息为：\n')
+                msg = msg.replace('\n','\n>')
+                data = {"msgtype": "markdown","markdown": {"content":title + "\n>" + msg}}
+                url = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key={}'.format(SendKey)
                 requests.post(url,headers=headers,json=data,verify=False)
             except Exception as e:
                 print("["+time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())+"]",e)
         else:
             print('未指定企业微信机器人的key，请配置！')
             exit()
+    elif send_type == "dingding":
+        if SendKey != "":
+            try:
+                headers = {
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36',
+                    'Content-Type': "application/json"
+                }
+                msg = msg.replace('更新消息为：','更新消息为：\n')
+                msg = msg.replace('\n','\n>')
+                data = {"msgtype": "markdown","markdown": {"content":title + "\n>" + msg}}
+                url = 'https://oapi.dingtalk.com/robot/send?access_token={}'.format(SendKey)
+                requests.post(url,headers=headers,json=data,verify=False)
+            except Exception as e:
+                print("["+time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())+"]",e)
+        else:
+            print('未指定钉钉机器人的key，请配置！')
+            exit()
     else:
-        print("指定的推送方式错误！可以选择：ServerChan(server酱）、Webhook(企业微信机器人)推送！")
-        exit()
-
+        print('send_type参数错误！')
 
 if __name__ == '__main__':
     db,conn = create_db()
     if not os.path.exists(tools_path):
         print("没有找到要监测的工具的列表文件！")
         exit()
-    while True:
-        with open(tools_path,mode='r',encoding='utf-8') as f:
-            for i in f:
-                api_url = i.replace('\n','')
-                tools_data = get_github_data(api_url)
-                # 排除工具404和访问异常的情况
-                if api_url not in list_404:
-                    tools_data = get_github_data(api_url)
-                    if tools_data == False:
-                        continue
-                else:
+    with open(tools_path,mode='r',encoding='utf-8') as f:
+        for i in f:
+            api_url = i.replace('\n','')
+
+            # 排除工具已经404的情况
+            query_404 = "select release_is_del from tools where tools_url='{}'".format(api_url.replace('https://api.github.com/repos/','https://github.com/'))
+            db.execute(query_404)
+            result_404 = db.fetchall()
+            if result_404 != []:
+                result_404 = result_404[0]
+                if result_404[0] == '1':
                     continue
-                query_sql = "select tools_name,commit_date_timestamp,tools_url,releases,releases_time,author from tools where tools_name='{}' and author='{}'".format(tools_data['tools_name'],tools_data['author'])
-                db.execute(query_sql)
-                query_result = db.fetchall()
-                if query_result != []:
-                    query_result = query_result[0]
-                if not query_result:
-                    print("数据库中不存在" + tools_data['tools_name'] + "工具，将会插入数据！")
-                    insert_sql = "insert into tools (tools_name,commit_date_timestamp,tools_url,releases,releases_time,author) values ('{}','{}','{}','{}','{}','{}')".format(tools_data['tools_name'],tools_data['commit_date_timestamp'],tools_data['tools_url'],tools_data['releases'],tools_data['releases_time'],tools_data['author'])
-                    # print(insert_sql)
-                    db.execute(insert_sql)
-                    conn.commit()
-                elif (int(query_result[1]) < int(tools_data['commit_date_timestamp'])):
-                    if tools_data['releases'] == "0":
-                        title = "**" + tools_data['tools_name'] + "**更新啦！"
-                        msg = "工具地址为：" + tools_data['tools_url'] + " ，\n更新消息为：" + tools_data['commit_message'] + " ，\n更新详情查看：" + tools_data['html_url'] + " ，\n工具不存在release版本！"
-                        update_sql = "update tools set commit_date_timestamp='{}' where tools_name='{}' and author='{}'".format(tools_data['commit_date_timestamp'],tools_data['tools_name'],tools_data['author'])
-                    elif int(query_result[4]) < int(tools_data['releases_time']):
-                        title = "**" + tools_data['tools_name'] + "**更新啦！"
-                        msg = "工具地址为：" + tools_data['tools_url'] + " ，\n更新消息为：" + tools_data['commit_message'] + " ，\n更新详情查看：" + \
-                              tools_data['html_url'] + " ，\n工具存在release版本，且版本也更新啦！release最新版本为：" + tools_data[
-                                  'releases'] + " ，\nrelease版本下载地址为：" + tools_data['releases_url']
-                        update_sql = "update tools set commit_date_timestamp='{}',releases='{}',releases_time='{}' where tools_name='{}' and author='{}'".format(
-                            tools_data['commit_date_timestamp'], tools_data['releases'], tools_data['releases_time'],
-                            tools_data['tools_name'],tools_data['author'])
-                    else:
-                        title = "**" + tools_data['tools_name'] + "**更新啦！"
-                        msg = "工具地址为：" + tools_data['tools_url'] + " ，\n更新消息为：" + tools_data['commit_message'] + " ，\n更新详情查看：" + \
-                              tools_data['html_url'] + " ，\n工具存在release版本，但release版本未更新！"
-                        print(tools_data['commit_date_timestamp'])
-                        update_sql = "update tools set commit_date_timestamp='{}' where tools_name='{}' and author='{}'".format(
-                            tools_data['commit_date_timestamp'], tools_data['tools_name'], tools_data['author'])
-                    send_server(title, msg)
-                    db.execute(update_sql)
-                    conn.commit()
+            # 访问GitHub的api，获取数据
+            tools_data = get_github_data(api_url)
+            # 排除访问错误情况。
+            if tools_data == False:
+                continue
+
+            # 如果项目404，这写入数据库，并且跳过。
+            elif tools_data == 404:
+                title = "！！！项目404告警！！！"
+                msg = '工具地址：' + api_url.replace('https://api.github.com/repos/','https://github.com/') + " ，\n监测地址：" + api_url
+                update_sql = "update tools set release_is_del='1' where tools_url='{}'".format(api_url.replace('https://api.github.com/repos/','https://github.com/'))
+                send_server(title, msg)
+                db.execute(update_sql)
+                conn.commit()
+                continue
+
+            # 查询并判断
+            query_sql = "select tools_name,commit_date_timestamp,tools_url,releases,releases_time,author from tools where tools_name='{}' and author='{}'".format(tools_data['tools_name'],tools_data['author'])
+            db.execute(query_sql)
+            query_result = db.fetchall()
+            if query_result != []:
+                query_result = query_result[0]
+            if not query_result:
+                print("数据库中不存在" + tools_data['tools_name'] + "工具，将会插入数据！")
+                insert_sql = "insert into tools (tools_name,commit_date_timestamp,tools_url,releases,releases_time,author,is_del,release_is_del) values ('{}','{}','{}','{}','{}','{}','0','0')".format(tools_data['tools_name'],tools_data['commit_date_timestamp'],tools_data['tools_url'],tools_data['releases'],tools_data['releases_time'],tools_data['author'])
+                # print(insert_sql)
+                db.execute(insert_sql)
+                conn.commit()
+            elif (int(query_result[1]) < int(tools_data['commit_date_timestamp'])):
+                if tools_data['releases'] == "0":
+                    title = "**" + tools_data['tools_name'] + "**更新啦！"
+                    msg = "工具地址为：" + tools_data['tools_url'] + " ，\n更新消息为：" + tools_data['commit_message'] + " ，\n更新详情查看：" + tools_data['html_url'] + " ，\n工具不存在release版本！"
+                    update_sql = "update tools set commit_date_timestamp='{}' where tools_name='{}' and author='{}'".format(tools_data['commit_date_timestamp'],tools_data['tools_name'],tools_data['author'])
                 elif int(query_result[4]) < int(tools_data['releases_time']):
                     title = "**" + tools_data['tools_name'] + "**更新啦！"
-                    msg = "该工具没有commits，但release版本更新啦！release最新版本为：" + tools_data[
-                                  'releases'] + " ，下载地址为：" + tools_data['releases_url']
-                    update_sql = "update tools set releases='{}',releases_time='{}' where tools_name='{}' and author='{}'".format(tools_data['releases'], tools_data['releases_time'], tools_data['tools_name'], tools_data['author'])
-                    send_server(title, msg)
-                    db.execute(update_sql)
-                    conn.commit()
-        time.sleep(60*3)
+                    msg = "工具地址为：" + tools_data['tools_url'] + " ，\n更新消息为：" + tools_data['commit_message'] + " ，\n更新详情查看：" + \
+                          tools_data['html_url'] + " ，\n工具存在release版本，且版本也更新啦！release最新版本为：" + tools_data[
+                              'releases'] + " ，\nrelease版本下载地址为：" + tools_data['releases_url']
+                    update_sql = "update tools set commit_date_timestamp='{}',releases='{}',releases_time='{}' where tools_name='{}' and author='{}'".format(
+                        tools_data['commit_date_timestamp'], tools_data['releases'], tools_data['releases_time'],
+                        tools_data['tools_name'],tools_data['author'])
+                else:
+                    title = "**" + tools_data['tools_name'] + "**更新啦！"
+                    msg = "工具地址为：" + tools_data['tools_url'] + " ，\n更新消息为：" + tools_data['commit_message'] + " ，\n更新详情查看：" + \
+                          tools_data['html_url'] + " ，\n工具存在release版本，但release版本未更新！"
+                    # print(tools_data['commit_date_timestamp'])
+                    update_sql = "update tools set commit_date_timestamp='{}' where tools_name='{}' and author='{}'".format(
+                        tools_data['commit_date_timestamp'], tools_data['tools_name'], tools_data['author'])
+                send_server(title, msg)
+                db.execute(update_sql)
+                conn.commit()
+            elif int(query_result[4]) < int(tools_data['releases_time']):
+                title = "**" + tools_data['tools_name'] + "**更新啦！"
+                msg = "该工具没有commits，但release版本更新啦！release最新版本为：" + tools_data[
+                    'releases'] + " ，下载地址为：" + tools_data['releases_url']
+                update_sql = "update tools set releases='{}',releases_time='{}' where tools_name='{}' and author='{}'".format(tools_data['releases'], tools_data['releases_time'], tools_data['tools_name'], tools_data['author'])
+                send_server(title, msg)
+                db.execute(update_sql)
+                conn.commit()
+            elif int(query_result[4]) != 0 and int(tools_data['releases_time']) == 0:
+                title = "**" + tools_data['tools_name'] + "**工具的release文件被删除！"
+                msg = "该工具release文件被删除，上一个release版本为：" + query_result[3] + " ，项目地址：" + query_result[2]
+                update_sql = "update tools set release_is_del='1' where tools_name='{}' and author='{}'".format(tools_data['tools_name'], tools_data['author'])
+                send_server(title, msg)
+                db.execute(update_sql)
+                conn.commit()
